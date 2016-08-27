b935b01c8dea513f329fd69abf3367ace4de0700
Kiran Gopalakrishnan <kirangplkrishnan@gmail.com>
2016-08-27 15:11:35 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 /**
  * Created by 1691618 on 8/27/2016.
  */
+$(document).readyState(function(e){
+    $(".typebox").on('submit', function(e){
+        e.preventDefault();
+        var em = $("#mc-email").val();
+        $.ajax({
+            type: "POST", url: "subscribe.php", dataType: "json", data: {email: em}, success: function (response) {
+                var result = JSON.parse(response);
+            }
+        });
+    });
+});
